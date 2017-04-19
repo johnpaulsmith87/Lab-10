0f47b7e83169e1ac1c61890ab73b5649eba46c55
johnpaulsmith87 <johnpaul.smith@gmail.com>
2017-04-19 12:01:51 -0400

    Lab 10 first commit

---
+++ Lab 10/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab_10.RestaurantReviewService;
+using System.Threading.Tasks;
+
+namespace Lab_10.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<ActionResult> Review()
+        {
+            var reviewer = new RestaurantReviewServiceClient();
+            var restaurants = await reviewer.GetRestaurantsByRatingAsync(0);
+            var jsonRestaurants = restaurants.Select(resto => new Models.RestaurantInfo(resto));
+            return Json(jsonRestaurants, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public async Task<ActionResult> UpdateReview(string restInfo)
+        {
+            if (!string.IsNullOrWhiteSpace(restInfo))
+            {
+                var restaurantInfo = System.Web.Helpers.Json.Decode<Models.RestaurantInfo>(restInfo);
+                var info = new RestaurantInfo()
+                {
+                    Name = restaurantInfo.Name,
+                    Summary = restaurantInfo.Summary,
+                    Rating = restaurantInfo.Rating,
+                    Location = new Address()
+                    {
+                        City = restaurantInfo.Location.City,
+                        Street = restaurantInfo.Location.Street,
+                        PostalCode = restaurantInfo.Location.PostalCode,
+                        Province = restaurantInfo.Location.Province                   
+                    }
+                };
+                var reviewer = new RestaurantReviewServiceClient();
+                if (await reviewer.SaveRestaurantAsync(info))
+                {
+                    return Json("Update successful.");
+                }
+            }
+            return Json("No data received.");
+        }
+    }
+}
---
+++ Lab 10/Models/RestaurantInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab_10.Models
+{
+    public class RestaurantInfo
+    {
+        public RestaurantInfo()
+        {
+
+        }
+        public RestaurantInfo(RestaurantReviewService.RestaurantInfo restaurant)
+        {
+            Name = restaurant.Name;
+            Location = new Address()
+            {
+                Street = restaurant.Location.City,
+                City = restaurant.Location.City,
+                PostalCode = restaurant.Location.PostalCode,
+                Province = restaurant.Location.PostalCode
+            };
+            Summary = restaurant.Summary;
+            Rating = restaurant.Rating;
+
+        }
+        public string Name { get; set; }
+        public Address Location { get; set; }
+        public string Summary { get; set; }
+        public int Rating { get; set; }
+    }
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+    }
+
+
+
+}
